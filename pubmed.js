3be93c1f0e29254c5f8b162abe91bfe862fda093
bsamirraj <amir.raj@brainstation23.com>
2021-04-26 18:09:46 +0600

    get all article list and fuzzy name matching

--- pubmed.js
+++ pubmed.js
@@ -1,4 +1,5 @@
 const puppeteer = require("puppeteer");
+const fuzz = require('fuzzball');
 
 (async () => {
   const browser = await puppeteer.launch({ headless: false });
@@ -6,11 +7,77 @@ const puppeteer = require("puppeteer");
   await page.goto("https://pubmed.ncbi.nlm.nih.gov/");
 
   //await page.waitForTimeout(5000);
-  await page.waitForSelector("#account_login");
-  //await page.waitForSelector("#set-base-url usa-button header-button");
-  //await page.type(".term-input tt-input","just checkig");
-  await page.click("#account_login");
-  
 
-  //await browser.close();
+  await page.waitForSelector("#id_term");
+  await page.type("#id_term", "karen voigt");
+  await page.click(".search-btn");
+
+  await page.waitForTimeout(1000);
+  // let singleArticle = await page.evaluate(() => {
+  //   let el = document.querySelector("#full-view-heading")
+  //   return el ? 'single article available' : ""
+  // });
+
+  // // if(singleArticle.length >1){
+  // //   console.log(singleArticle);
+  // //   return
+  // // }
+
+  let loadMore = true;
+  while (loadMore) {
+    let count = 1;
+    const selector = '.load-button';
+    await page.waitForTimeout(1000);
+
+    if ((await page.$(selector)) !== null  && count<=5) {
+      await page.click(selector);
+      count++;
+    } else {
+      loadMore = false;
+    }
+  }
+
+  const getArticleIds = await page.evaluate(
+    () => Array.from(
+      document.querySelectorAll("a[class='docsum-title']"),
+      a => a.getAttribute('href')
+    )
+  );
+
+  let authorName = '';
+  for (i=0; i< getArticleIds.length ; i++){
+    let ratio = -100; 
+
+    const page = await browser.newPage();
+    await page.goto("https://pubmed.ncbi.nlm.nih.gov"+getArticleIds[i]);
+    await page.waitForSelector(".inline-authors > div.authors > div.authors-list > span > a");
+    
+     
+    const hcpNameList = await page.evaluate(
+      () => Array.from(
+        document.querySelectorAll(".inline-authors > div.authors > div.authors-list > span > a"),
+        a => a.innerHTML
+      )
+    );
+    hcpNameList.forEach(name=>{
+      const tempRatio = fuzz.ratio("karen voigt",name);
+      if (tempRatio >ratio)
+      {
+        ratio = tempRatio;
+        authorName = name;
+      }
+    });
+   
+    page.close();
+    break;
+  }
+   
+
+  console.log('author name is ',authorName);
+// console.log('The ids are ',getArticleIds.length);
+ console.log('The ratio is ',fuzz.ratio("karen voigt","voigt karen"));
+
+
+  await browser.close();
+
 })();
