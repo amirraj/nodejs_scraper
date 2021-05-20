62977a0161f6706b4251b34ee7846349a1f30223
amirraj <raj.amir@outlook.com>
2021-05-20 18:53:32 +0600

    applied filter to remove redundant links

--- robust.scrapper.js
+++ robust.scrapper.js
@@ -6,15 +6,15 @@ var os = require("os");
 (async () => {
   const browser = await puppeteer.launch({ headless: false, defaultViewport: null,ignoreHTTPSErrors: true, });
   const page = await browser.newPage();
-  await page.goto("https://www.pubpharm.de/vufind/Search/Results?lookfor=Karen&limit=10&type=AllFields");
+  //await page.goto("https://www.pubpharm.de/vufind/Search/Results?lookfor=Karen&limit=10&type=AllFields");
   //await page.goto("https://sciencedirect.com/search?authors=Karen");
-  //await page.goto("https://scholar.google.com/scholar?hl=en&as_sdt=0%2C5&q=Karen+C+Johnson&btnG=");
+  await page.goto("https://scholar.google.com/scholar?hl=en&as_sdt=0%2C5&q=Karen+C+Johnson&btnG=");
   
   await page.waitForTimeout(5000);
   const hcpArticleList = await page.evaluate(
     () => Array.from(
       document.querySelectorAll("a"),
-      a => ({link : a.href, title : a.innerText.trim()})
+      a => ({link : a.href, title : a.innerText.trim().replace(/\s\s+/g, ' ')})
     )
   );
   
